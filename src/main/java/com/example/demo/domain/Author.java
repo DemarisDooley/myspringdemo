9a34105d22aef2f95f95454d3ad0e54ac20e02fa
DemarisDooley <dooleydemaris@gmail.com>
2024-02-23 19:22:11 -0600

    Create Author domain

---
+++ src/main/java/com/example/demo/domain/Author.java
@@ -0,0 +1,6 @@
+package com.example.demo.domain;
+
+public class Author {
+    // Initially, the file name "Author" is red --> it means this file has not been added to github
+    // Then, let's manually add it
+}
